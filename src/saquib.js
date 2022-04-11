b2ce7b75735b7ca309cdca95d6d2b6a439d35307
Saquib Akhtar <saquibmohammad38@gmail.com>
2022-04-11 16:22:35 +0530

    spit

---
+++ src/saquib.js
@@ -0,0 +1,22 @@
+function saquib(num) {
+    if (num % 2 == 0) {
+        return true
+
+
+
+    }
+    else {
+        return false
+        
+
+     
+
+    }
+
+
+
+  
+
+
+}
+console.log(saquib(3))
